cd97104dc01e2719dd071d344ff8199849dfa585
Jhon Gonzalez <66422237+jhongonzalezweb@users.noreply.github.com>
2022-04-17 16:13:58 -0400

    Update js.js

--- js/js.js
+++ js/js.js
@@ -2,7 +2,15 @@ function myFunction() {
     var x = document.getElementById("carouselExampleSlidesOnly");
     if (x.style.display === "none") {
         x.style.display = "block";
+        var ocultarVideo = document.getElementById("ocultaVideo");
+        ocultarVideo.innerHTML = "Ocultar"
+
+        var botoncito = document.getElementById("botoncito");
+        botoncito.style.marginLeft = "48%";
+        
     } else {
         x.style.display = "none";
+        var ocultarVideo = document.getElementById("ocultaVideo");
+        ocultarVideo.innerHTML = "Ver video.. InstaBot con Python!!"
     }
 }
